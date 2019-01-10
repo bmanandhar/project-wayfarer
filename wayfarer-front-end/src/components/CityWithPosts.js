34a75d1a66bf03eb956c7b3a5cc2fc31df072627
harrystacey123 <harrystacey@live.co.uk>
2019-01-10 14:20:49 -0800

    finishing post component

--- wayfarer-front-end/src/components/CityWithPosts.js
+++ wayfarer-front-end/src/components/CityWithPosts.js
@@ -7,11 +7,15 @@ class CityWithPosts extends Component {
         return(
             <div className="city-with-posts-div">
                 <div className="city-country-img">
-                    <div>
-                        <h1>London</h1>
-                        <h3>United Kingdom</h3>
+                    <div className='london-uk'>
+                        <h1 className="city">London</h1>
+                        <h3 className="country">United Kingdom</h3>
                     </div>
                     <img className="london-large-img" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg"></img>
+                </div>
+                <div className="posts-and-button">
+                    <h2 className='posts'>Posts</h2>
+                    <button className="plus-button">+</button>
                 </div>
                 <div className="posts-div">
                     <Posts />
