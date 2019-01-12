7938d9e96649b3b42576671e339e9a9d7db2b451
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-01-11 16:41:53 -0800

    adds about page

--- wayfarer-front-end/src/components/App.js
+++ wayfarer-front-end/src/components/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header/Header.js';
 import Landing from './Landing';
+import About from './About.js';
 
 //import CitiesList from './CitiesList';
 //import CityWithPosts from './CityWithPosts';
@@ -15,7 +16,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Landing />
-        
+        <About />
 
       </div>
     );
