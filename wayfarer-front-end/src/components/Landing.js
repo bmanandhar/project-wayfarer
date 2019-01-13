1021eb943eb58f3744db7654d43c7165b08acfc2
harrystacey123 <harrystacey@live.co.uk>
2019-01-13 13:04:15 -0800

    worked on back end to populate pictuers

--- wayfarer-front-end/src/components/Landing.js
+++ wayfarer-front-end/src/components/Landing.js
@@ -1,26 +1,49 @@
 import React, {Component} from "react";
 import { Modal, Button, Grid, Row, Col, Carousel } from "react-bootstrap"
+import Axios from "axios";
 
 const imgURL = "https://upload.wikimedia.org/wikipedia/commons/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg"
 const height= 480, width= 640
 
 export default class Landing extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      cities:[]
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:8001/cities/all')
+    .then(response => {
+      this.setState({
+        cities: response.data.cities
+      })
+    })
+  }
+
     render() {
+
+      var citiesImg = this.state.cities.map((city, i) => {
         return(
-<React.Fragment >
-  <Carousel>
-    <Carousel.Item>
-      <img width={width} height={height} alt="widthxheight" src={imgURL} />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img width={width} height={height} alt="widthxheight" src={imgURL} />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img width={width} height={height} alt="widthxheight" src={imgURL} />
-    </Carousel.Item>
-  </Carousel>
-</React.Fragment>
+          <Carousel.Item>
+            <div key={i}>
+            <img src={`${city.image}`} />
+            </div>
+          </Carousel.Item>
+          
+        )
+
+      })
+
+        return(
+          <React.Fragment >
+            <Carousel>
+              {citiesImg}
+            </Carousel>
+          </React.Fragment>
         );
+      
     }
 }
