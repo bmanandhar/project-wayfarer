b6820afff6f6b9471ed2179e077a25821c637555
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-01-13 17:12:30 -0800

    adds changes

--- wayfarer-front-end/src/components/About.js
+++ wayfarer-front-end/src/components/About.js
@@ -5,39 +5,39 @@ import './App.css';
 class About extends Component {
     render() {
         const para = [
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
+            {topic: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. \
             It has survived not only five centuries, but also the leap into electronic typesetting, \
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset \
-            sheets containing Lorem Ipsum passages.", 
+            sheets containing Lorem Ipsum passages."}, 
 
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
+            {topic: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. \
             It has survived not only five centuries, but also the leap into electronic typesetting, \
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset \
-            sheets containing Lorem Ipsum passages.",
+            sheets containing Lorem Ipsum passages."},
 
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
+            {topic: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. \
             It has survived not only five centuries, but also the leap into electronic typesetting, \
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset \
-            sheets containing Lorem Ipsum passages."
+            sheets containing Lorem Ipsum passages."}
         ];
         return(
             <Jumbotron>
                 <h3>Wayfarer is ...</h3>
                 <main>
                     <div className='left-col'>
-                    <h4>Topic one</h4>
-                        <p>{ para[0] }</p>
+                        <h4>Topic one</h4>
+                        <p>{ para[0].topic }</p>
                     </div>
                     <div className='middle-col'>
-                    <h4>Topic two</h4>
-                        <p>{ para[1] }</p>
+                        <h4>Topic two</h4>
+                        <p>{ para[1].topic }</p>
                     </div>
                     <div className='right-col'>
                     <h4>Topic three</h4>
-                        <p>{ para[2] }</p>
+                        <p>{ para[2].topic }</p>
                     </div>
                 </main>
             </Jumbotron>
